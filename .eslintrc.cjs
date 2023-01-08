ae4b5a42a546f889e8d8a353402efd72b69f60a7
Castle <castlerock88@protonmail.com>
2023-01-08 13:17:37 -0500

    project init

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "jsx-a11y", "prettier"],
+  rules: {
+    "jsx-a11y/rule-name": 2,
+    "jsx-a11y/aria-role": true,
+    "prettier/prettier": "error",
+  },
+};
