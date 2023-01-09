b97e876b9359372f653a279555cd16e81ad27bf3
Castle <castlerock88@protonmail.com>
2023-01-09 14:52:25 -0500

    hardware section

---
+++ utils/hardware.ts
@@ -0,0 +1,34 @@
+export const hardware = [
+  {
+    name: "BentoBox",
+    href: "https://scrubtier.co.uk/products/bentobox",
+  },
+  {
+    name: "Flatbox",
+    href: "https://github.com/jfedor2/flatbox",
+  },
+  {
+    name: "Zandatsu",
+    href: "https://github.com/Sharqo78/Zandatsu",
+  },
+  {
+    name: "Chastetiddy",
+    href: "https://github.com/chastetiddy/chastity_device",
+  },
+  {
+    name: "BubbleBox",
+    href: "https://github.com/UMS-Ultra/BubbleBox",
+  },
+  {
+    name: "CrushCounter",
+    href: "https://github.com/SleepUnit/CrushCounter",
+  },
+  {
+    name: "Mavercade",
+    href: "https://www.etsy.com/shop/Mavercade",
+  },
+  {
+    name: "Ultra-Arcade",
+    href: "https://github.com/Ultra-Arcade/open-ua-cnc",
+  },
+];
