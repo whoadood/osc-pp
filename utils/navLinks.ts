0ecd45d76c54e5d92aa580e1351f9955e34594fd
Castle <castlerock88@protonmail.com>
2023-01-08 23:08:45 -0500

    landing page

---
+++ utils/navLinks.ts
@@ -0,0 +1,17 @@
+export const navLinks = [
+  {
+    title: "Showcase",
+    href: "/showcase",
+    external: false,
+  },
+  {
+    title: "Docs",
+    href: "https://gp2040-ce.info/#/",
+    external: true,
+  },
+  {
+    title: "FAQ",
+    href: "/FAQ",
+    external: false,
+  },
+];
