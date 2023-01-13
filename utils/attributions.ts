f4a37d73a1de77b0ecfdad568d2c9e56ff82c662
Castle <castlerock88@protonmail.com>
2023-01-13 00:03:39 -0500

    attribution list

---
+++ utils/attributions.ts
@@ -0,0 +1,15 @@
+export type Attribution = {
+  name: string;
+  href: string;
+};
+
+export const attributions: Attribution[] = [
+  {
+    name: "FeralAI",
+    href: "https://github.com/FeralAI",
+  },
+  {
+    name: "royyimzy25414 - Vecteezy",
+    href: "https://www.vecteezy.com/vector-art/7872288-abstract-digital-background-with-technology-circuit-board-texture-electronic-motherboard-illustration-communication-and-engineering-concept-vector-illustration",
+  },
+];
