1d9813d5bdcb0794b26b7159b3ec60a21018e89f
Castle <castlerock88@protonmail.com>
2023-01-12 23:23:42 -0500

    add vendors to vendor list

--- utils/vendors.ts
+++ utils/vendors.ts
@@ -1,10 +1,7 @@
 export type Vendor = {
   icon: string | null; // 'https://www.my-icon.com/project' your creator icon url - use null if n/a
   author: string | null; // your name/handle - use null if n/a
-  image: {
-    light: string | null; // /<YOUR_FILE_NAME>.<YOUR_FILE_EXTENSION> your project image - should be located in the public/projects directory - use null if n/a
-    dark: string | null;
-  } | null;
+  image: string | null; // /<YOUR_FILE_NAME>.<YOUR_FILE_EXTENSION> your project image - should be located in the public/projects directory - use null if n/a
   name: string; // name of project
   href: string; // link to project website
 };
@@ -12,33 +9,27 @@ export const vendors: Vendor[] = [
   {
     icon: "https://cdn.shopify.com/s/files/1/0612/9211/8210/files/ScrubTier_Hor_Logo_White_220x.png?v=1647889827",
     author: "SCRUBTIER",
-    image: {
-      light:
-        "https://cdn.shopify.com/s/files/1/0612/9211/8210/products/imageshru016.jpg?v=1667149831&width=1100",
-      dark: "https://cdn.shopify.com/s/files/1/0612/9211/8210/products/imageshru004.jpg?v=1667149831&width=1100",
-    },
+    image:
+      "https://cdn.shopify.com/s/files/1/0612/9211/8210/products/imageshru016.jpg?v=1667149831&width=1100",
+
     name: "BentoBox",
     href: "https://scrubtier.co.uk/products/bentobox",
   },
   {
     icon: null,
     author: "jfedor2",
-    image: {
-      light:
-        "https://raw.githubusercontent.com/jfedor2/flatbox/master/hardware-rev2/images/Flatbox-rev2b-finished-product.jpg",
-      dark: null,
-    },
+    image:
+      "https://raw.githubusercontent.com/jfedor2/flatbox/master/hardware-rev2/images/Flatbox-rev2b-finished-product.jpg",
+
     name: "Flatbox",
     href: "https://github.com/jfedor2/flatbox",
   },
   {
     icon: null,
     author: "Sharqo78",
-    image: {
-      light:
-        "https://raw.githubusercontent.com/Sharqo78/Zandatsu/master/Assets/zandatsu_image0.png",
-      dark: null,
-    },
+    image:
+      "https://raw.githubusercontent.com/Sharqo78/Zandatsu/master/Assets/zandatsu_image0.png",
+
     name: "Zandatsu",
     href: "https://github.com/Sharqo78/Zandatsu",
   },
@@ -52,11 +43,8 @@ export const vendors: Vendor[] = [
   {
     icon: null,
     author: "UMS-Ultra",
-    image: {
-      light:
-        "https://raw.githubusercontent.com/UMS-Ultra/BubbleBox/main/Images/BubbleBox%20Black%20and%20Light%20Grey%20Hex%20Keycaps.jpg",
-      dark: null,
-    },
+    image:
+      "https://raw.githubusercontent.com/UMS-Ultra/BubbleBox/main/Images/BubbleBox%20Black%20and%20Light%20Grey%20Hex%20Keycaps.jpg",
     name: "BubbleBox",
     href: "https://github.com/UMS-Ultra/BubbleBox",
   },
@@ -70,23 +58,36 @@ export const vendors: Vendor[] = [
   {
     icon: "https://i.etsystatic.com/isla/eeaa8c/51715873/isla_500x500.51715873_rahh64eq.jpg?version=0",
     author: "Mavercade",
-    image: {
-      light:
-        "https://i.etsystatic.com/31891698/r/il/9fc21d/3629429007/il_794xN.3629429007_1sxc.jpg",
-      dark: null,
-    },
+    image:
+      "https://i.etsystatic.com/31891698/r/il/9fc21d/3629429007/il_794xN.3629429007_1sxc.jpg",
+
     name: "Mavercade",
     href: "https://www.etsy.com/shop/Mavercade",
   },
   {
     icon: "https://cdn.shopify.com/s/files/1/0578/5770/3983/files/suble.png?v=1660245244&width=500",
     author: "Ultra-Arcade",
-    image: {
-      light:
-        "https://cdn.shopify.com/s/files/1/0578/5770/3983/products/Hitbox_Mod5_main.png?v=1661192291",
-      dark: null,
-    },
+    image:
+      "https://cdn.shopify.com/s/files/1/0578/5770/3983/products/Hitbox_Mod5_main.png?v=1661192291",
+
     name: "Ultra-Arcade",
     href: "https://shop.ultraarcadebh.com.br/",
+  },
+  {
+    icon: "https://i.etsystatic.com/isla/4b663e/37496037/isla_500x500.37496037_lgwwegbr.jpg?version=0",
+    author: "thnikk",
+    image:
+      "https://i.etsystatic.com/10408775/r/il/09b343/4181633957/il_794xN.4181633957_trxq.jpg",
+
+    name: "Fightboard",
+    href: "https://www.etsy.com/shop/thnikk?ref=seller-platform-mcnav&section_id=41068960",
+  },
+  {
+    icon: "https://cdn.shopify.com/s/files/1/0374/7499/2261/files/New-MA-Logo_Tiny_180x.png?v=1644347668",
+    author: "Mister-Addon",
+    image:
+      "https://cdn.shopify.com/s/files/1/0374/7499/2261/products/ReflexFightingBoard_1024x1024@2x.jpg?v=1673333907",
+    name: "Mister-Addon",
+    href: "https://misteraddons.com/collections/parts/products/reflex-encode-fighting-board",
   },
 ];
